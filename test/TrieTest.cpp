9697cc309ef72fe30b009743e17566d1ff6626f4
Ian Macalinao <me@ian.pw>
2014-08-31 23:58:40 -0500

    Add aspen truth test

--- test/TrieTest.cpp
+++ test/TrieTest.cpp
@@ -8,6 +8,7 @@ int main() {
   trie->add("asparagus");
   trie->add("aspen");
   cout << "This should be true: " << (trie->search("asparagus") != 0) << endl;
+  cout << "This should be true: " << (trie->search("aspen") != 0) << endl;
   cout << "This should be false: " << trie->search("aaragus") << endl;
   cout << "This should be false: " << trie->search("asparaguse") << endl;
   cout << "This should be false: " << trie->search("asparagu") << endl;
